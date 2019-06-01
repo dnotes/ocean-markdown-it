8e49d231a45d080d81d217b68ae6261a208b6304
David Hunt <git@dnotes.net>
2019-06-01 10:44:51 -0700

    added basic typography replacements

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
 'use strict'
+
+// Load plugins
+const typography = require('markdown-it-replacements')
+
+// Set typographic replacements
+typography.replacements.push({
+  name: 'copyright',
+  re: /\([cC]\)/mg,
+  sub: '©',
+  default: true,
+})
+typography.replacements.push({
+  name: 'trademark',
+  re: /\((?:tm|TM)\)/mg,
+  sub: '™',
+  default: true,
+})
+typography.replacements.push({
+  name: 'registered',
+  re: /\([rR]\)/mg,
+  sub: '®',
+  default: true,
+})
+typography.replacements.push({
+  name: 'mdash',
+  re: /(^|[^-])--([^-]|$)/mg,
+  sub: '$1\u2014$2',
+  default: true,
+})
+typography.replacements.push({
+  name: 'mdash',
+  re: /(^|[^-\s])--([^-\s]|$)/mg,
+  sub: '$1\u2014$2',
+  default: true,
+})
+
+// Load markdown
 const md = require('markdown-it')('commonmark', {
   typographer: true,
 })
+  .enable(['linkify', 'smartquotes', 'replacements'])
   .use(require('markdown-it-footnote'))
+  .use(typography, {
+    plusminus: false,
+    ellipsis: false,
+    ndash: false,
+  })
 
 module.exports = md

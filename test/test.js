faeabb1d409455fe207957ba3a0722e61c541feb
David Hunt <git@dnotes.net>
2019-06-03 20:34:01 -0700

    whitespace fix

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ var generate = require('markdown-it-testgen')
 
 /* eslint-env mocha */
 
-describe('Tests for ocean-markdown-it wrapper', function () {
+describe('Tests for ocean-markdown-it wrapper', function() {
   var md = require('../')
   generate(path.join(__dirname, 'fixtures/definitions.txt'), { header: true }, md)
 })
